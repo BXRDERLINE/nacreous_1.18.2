0c43312b77ba647e531fcc9594f84250122ace86
BXRDERLINE <beyekone@gmail.com>
2022-09-23 17:00:24 +0800

    test v0.1

---
+++ src/main/java/net/bxrderline/nacreous/NacreousClient.java
@@ -0,0 +1,10 @@
+package net.bxrderline.nacreous;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NacreousClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
