9722bd8d2575d041e72167ff710568dfad1bf0e1
BXRDERLINE <beyekone@gmail.com>
2022-09-23 17:05:05 +0800

    added testing comment

--- src/main/java/net/bxrderline/nacreous/Nacreous.java
+++ src/main/java/net/bxrderline/nacreous/Nacreous.java
@@ -3,7 +3,7 @@ package net.bxrderline.nacreous;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// For testing purposes
 public class Nacreous implements ModInitializer {
 
 	public static final String MOD_ID = "nacreous";
